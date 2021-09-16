9e1bcffbfffad66e5a3687f98bac72408486a22d
andrew-cmdltt <menwhohas2279@gmail.com>
2021-09-16 23:10:38 +0300

    statistic page

--- src/components/StatisticPage/StatisticPage.tsx
+++ src/components/StatisticPage/StatisticPage.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import styles from './statisticpage.module.css';
 import {DayOfWeekBlock} from "./DayOfWeekBlock";
-import {log} from "util";
+import {ScalesBlock} from "./ScalesBlock";
 
 export function StatisticPage() {
-    console.log("statistic page")
     return (
         <div className={styles.statisticPage}>
             <DayOfWeekBlock />
-            ScalesBlock
-            Counters
+            <ScalesBlock />
         </div>
     );
 }
